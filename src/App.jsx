c4461a680e62a10d9f4346e6ac9ab8d6ea76f794
PhuocLoc <lephuocloc1729@gmail.com>
2023-08-22 12:09:34 +0700

    add CRUD to the web
    implement create, read,  update, delete data from the database

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import { AddItem, Content, Footer, Header, SearchItem } from "./components";
+import apiRequest from "./apiRequest";
 function App() {
   const API_URL = "http://localhost:3500/items";
 
@@ -39,24 +40,53 @@ function App() {
     setSearchText(searchText);
   };
 
-  const addItem = (addedItem) => {
-    let newItems = [
-      ...items,
-      { id: items.length + 1, checked: false, item: addedItem },
-    ];
-    setItems(newItems);
+  const addItem = async (addedItem) => {
+    let newItem = { id: items.length + 1, checked: false, item: addedItem };
+    setItems([...items, newItem]);
+
+    const postOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newItem),
+    };
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) {
+      setFetchError(result);
+    }
   };
 
-  const deleteItem = (id) => {
+  const deleteItem = async (id) => {
     let newItems = items.filter((item) => item.id !== id);
     setItems(newItems);
+
+    const deleteOptions = {
+      method: "DELETE",
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) setFetchError(result);
   };
 
-  const completeTask = (id) => {
-    let newItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item
+  const completeTask = async (id) => {
+    setItems(
+      items.map((item) =>
+        item.id === id ? { ...item, checked: !item.checked } : item
+      )
     );
-    setItems(newItems);
+    let completeItem = items.filter((item) => item.id === id);
+
+    const updateOptions = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ checked: !completeItem[0].checked }),
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   };
 
   return (
--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -3,7 +3,13 @@ import { useState } from "react";
 const AddItem = ({ onAdd }) => {
   const [addedItem, setAddedItem] = useState("");
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onAdd(addedItem);
+        setAddedItem("");
+      }}
+    >
       <input
         type="text"
         placeholder="Add item"
@@ -12,14 +18,7 @@ const AddItem = ({ onAdd }) => {
         onChange={(e) => setAddedItem(e.target.value)}
         required
       />
-      <button
-        className="px-6 py-2 text-xl rounded-full bg-slate-700 text-white"
-        onClick={(e) => {
-          e.preventDefault();
-          onAdd(addedItem);
-          setAddedItem("");
-        }}
-      >
+      <button className="px-6 py-2 text-xl rounded-full bg-slate-700 text-white">
         Add
       </button>
     </form>
